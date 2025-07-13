b8749356d24f7d173cca45119c2302fa7187b77e
Alex <revooms@users.noreply.github.com>
2025-07-13 19:24:55 +0200

    Remove inventory addon
    Add temp background assets
    Update playground
    Update player, show y-force
    Ignore temp images
    Add F4 toggle player debug info
    Refactor player controller
    Can now steer in air
    Debug infos updated
    Add parallax

--- example/theme/font/ttf/Abaddon.txt
+++
@@ -1,5 +0,0 @@
-Abaddon Font
-
-Licensed under Creative Commons Attribution 3.0.
-
-https://caffinate.itch.io/abaddon
--- prefabs/player/player.gd
+++ prefabs/player/player.gd
@@ -12,12 +12,25 @@ extends CharacterBody2D
 @export_group("Movement")
 @export var speed = 190
 @export var jumpPower = 370
+@export var max_jumps = 2
+var jumps: int = 0
 @export var gravity = 1000
 @export var knockBackForce = 150
 
 @export_group("Zoom")
 @export var zoom_levels = [0.25, 0.5, 1, 1.25, 1.5]
 var current_zoom_level = 0
+
+@export_category("Inventory")
+@export var generator: Resource
+@export var inventory_menu := NodePath()
+@export var inventory_tooltip := NodePath()
+@export var ground_items := NodePath()
+
+var _mouse_pressed := false
+
+@export_category("Debug")
+@export var show_debug_info: bool = true
 
 @onready var camera_2d = get_node("Camera2D")
 
@@ -33,12 +46,41 @@ var isFalling = false
 
 @onready var tilemap_layer: TileMapLayer = %TileMapLayer
 var original_tile_id: int
-var highlight_tile_id: int = 7  # Ersetzen Sie dies mit der ID Ihres Highlight-Tiles
+var highlight_tile_id: int = 7 # Ersetzen Sie dies mit der ID Ihres Highlight-Tiles
 var tilemaplayer_position: Vector2i
 
 func _ready() -> void:
 	release_mouse_cursor()
-	set_zoom(1)
+	set_zoom(2)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		_mouse_pressed = event.is_pressed()
+
+	if event.is_action(&"ui_cancel") && event.is_pressed():
+		get_node(inventory_menu).hide()
+
+	if event.is_action(&"menu_inventory") && event.is_pressed():
+		get_node(inventory_menu).visible = !get_node(inventory_menu).visible
+		get_node(inventory_tooltip).visible = false
+
+func _on_Generator_pressed():
+	var item_manager = get_node(ground_items)
+	for i in 8:
+		for x in generator.get_items():
+			item_manager.add_item(x, global_position)
+
+func _on_ItemPickup_area_entered(area: Area2D):
+	if area.is_in_group(&"ground_item") && !area.filter_hidden:
+		# Inventory? Inventory. Don't hard-code paths, kids.
+#		area.try_pickup($"../../../Inventory/Inventory/Inventory".inventory)
+		area.try_pickup(get_node(inventory_menu).main_inventory)
+
+func _on_inworld_inv_button_pressed(inventory_view: Control, inventory_name: String):
+		get_node(inventory_menu).open_inworld_inventory(inventory_view, inventory_name)
+
+func _on_items_item_clicked(item_node: Node):
+	_on_ItemPickup_area_entered(item_node)
 
 func debugmsg() -> String:
 	var msg = "Player Debug Info:"
@@ -49,6 +91,8 @@ func debugmsg() -> String:
 	msg += "\n"
 	msg += "pos2: %3.2f/%3.2f" % [tile_position.x, tile_position.y]
 	msg += "\n"
+	msg += "Y-Force: %3.2f" % [velocity.y]
+	msg += "\n"
 	msg += "currentDir: %d" % [currentDirection]
 	msg += "\n"
 	msg += "zoom: %3.2f" % [zoom_levels[current_zoom_level]]
@@ -58,6 +102,8 @@ func debugmsg() -> String:
 	msg += "isonceil: %s" % [is_on_ceiling()]
 	msg += "\n"
 	msg += "isjumping: %s" % [isJumping]
+	msg += "\n"
+	msg += "jumps: %d/%d" % [jumps, max_jumps]
 	msg += "\n"
 	msg += "isfalling: %s" % [isFalling]
 	msg += "\n"
@@ -74,7 +120,7 @@ func highlight_cell(_x: int, _y: int, duration: float = 2.0):
 	print(str(tile_data))
 
 	# Setzen Sie die Zelle auf den Highlight-Tile
-	tilemap_layer.set_cell(cell_coords,0, Vector2i(0,7))
+	tilemap_layer.set_cell(cell_coords, 0, Vector2i(0, 7))
 	
 	# Timer, um den Tile nach der gewünschten Zeit zurückzusetzen
 	var timer = Timer.new()
@@ -87,14 +133,12 @@ func highlight_cell(_x: int, _y: int, duration: float = 2.0):
 func _on_Timer_timeout(x: int, y: int):
 	# Setzen Sie die Zelle zurück auf den ursprünglichen Tile
 	# tilemap_layer.tilemap.set_cell(x, y, original_tile_id)
-	tilemap_layer.set_cell(Vector2i(x,y), 0, Vector2i(0,1))
-	queue_free()  # Optional: Timer entfernen
-
+	tilemap_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 1))
+	queue_free() # Optional: Timer entfernen
 
 func get_tilemaplayer_position() -> Vector2i:
 	tilemaplayer_position = tilemap_layer.local_to_map(tilemap_layer.to_local(self.position))
 	return tilemaplayer_position
-
 
 func detect_below() -> void:
 	pass
@@ -108,8 +152,6 @@ func get_player_tile() -> void:
 	else:
 		print("Zelle ", cell_coords, " ist leer.")
 	#print("pos %s - %s" % [str(self.position), str(cell_coords)])
-
-
 
 func paint_debug_msg() -> void:
 	%PlayerDebugInfo.text = debugmsg()
@@ -132,6 +174,10 @@ func _process(delta: float):
 			playerHealth = 100
 		print("new player health = ", playerHealth)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_debug"):
+		toggle_debug_info()
+
 func _physics_process(delta):
 	# player_health_update()
 	# Apply gravity if in the air
@@ -150,33 +196,35 @@ func _physics_process(delta):
 
 	# Player movement
 	var playerDirection = Input.get_axis("move_left", "move_right")
-	
+
+	if is_on_floor():
+		isFalling = false
+		isJumping = false
+		jumps = 0
+
 	# Check player is on the floor
-	if is_on_floor() and not isDead and not isHit:
-		
-		if Input.is_action_just_pressed("jump") and not playerAttacking:
-			velocity.y = - jumpPower
-			$AnimationPlayer.play("Jump")
-			isJumping = true
-		elif playerDirection and not playerAttacking:
+	if not isDead and not isHit:
+		if playerDirection:
 			move_horizontally(playerDirection)
-			isJumping = false
 		else:
 			idle_animation()
 
-		isJumping = false
-		isFalling = false
-	else:
-		if not isDead and not isHit:
+		if jumps < max_jumps:
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = - jumpPower
+				jumps += 1
+				isJumping = true
+				$AnimationPlayer.play("Jump")
+
+		if not is_on_floor():
 			if velocity.y > 0:
 				isFalling = true
 				isJumping = false
 			else:
 				isJumping = true
 				isFalling = false
-
 			$AnimationPlayer.play("Fall" if velocity.y > 0 else "Jump")
-			
+		
 	if Input.is_action_just_pressed("primary_weapon") and is_on_floor() and not isDead and not isHit:
 		playerAttacking = true
 		$AnimationPlayer.play("attackRight" if currentDirection > 0 else "attackLeft")
@@ -242,3 +290,11 @@ func _on_health_zone_body_entered(body):
 func _on_health_zone_body_exited(body):
 	if body.name == "Player":
 		health_increase = false
+
+func toggle_debug_info() -> void:
+	show_debug_info = !show_debug_info
+
+	if show_debug_info:
+		%DebugPanel.show()
+	else:
+		%DebugPanel.hide()
